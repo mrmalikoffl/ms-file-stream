288dd9ce9285a5bd455ce74683118f2ee8ccfba4
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-27 08:17:19 +0800

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -1,5 +1,5 @@
-# This file is a part of TG-FileStreamBot
-# Coding : Jyothis Jayanth [@EverythingSuckz]
+# This file is a part of ms-file-stream
+# Coding : Mr Malik [@mrmalik_offl]
 
 from pyrogram import filters
 from pyrogram.types import Message
