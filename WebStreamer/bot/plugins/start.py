22626d6e30741759a703616a006382e10f2ce528
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-27 09:39:53 +0800

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -4,10 +4,15 @@
 from pyrogram import filters
 from pyrogram.types import Message
 from WebStreamer.bot import StreamBot
-
+from vars import START_PIC
 
 @StreamBot.on_message(filters.command(["start", "help"]))
 async def start(_, m: Message):
     await m.reply(
         f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
     )
+    
+    if START_PIC:
+        await message.reply_photo(START_PIC, caption=txt, reply_markup=button)       
+    else:
+        await message.reply_text(text=txt, reply_markup=button, disable_web_page_preview=True)
