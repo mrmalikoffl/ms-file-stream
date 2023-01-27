2fd135658df4b8c730a1b5cf4a20736f87d7afc2
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-27 08:04:12 +0800

    Update generate_changelogs.sh

--- .github/workflows/generate_changelogs.sh
+++ .github/workflows/generate_changelogs.sh
@@ -3,9 +3,9 @@
 
 echo "## CHANGELOGS"
 
-last_commit=$(curl -s https://api.github.com/repos/EverythingSuckz/TG-FileStreamBot/tags | jq -r '.[0].commit.sha')
+last_commit=$(curl -s https://api.github.com/repos/mrmalikoffl/ms-file-stream/tags | jq -r '.[0].commit.sha')
 
-resp=$(curl -s https://api.github.com/repos/EverythingSuckz/TG-FileStreamBot/commits?per_page=5)
+resp=$(curl -s https://api.github.com/repos/mrmalikoffl/ms-file-stream/commits?per_page=5)
 data=$(echo "${resp}" | jq -c '.[]')
 while read -r i; do
     if [[ $i == $last_commit ]]; then
@@ -23,5 +23,5 @@ if [[ ! -z "$last_commit" ]]; then
     last_commit="main"
 fi
 
-compare_url="https://github.com/EverythingSuckz/TG-FileStreamBot/compare/$(echo $resp | jq -r '.[0].sha')...$last_commit"
+compare_url="https://github.com/mrmalikoffl/ms-file-stream/compare/$(echo $resp | jq -r '.[0].sha')...$last_commit"
 echo -e "- [.....]($compare_url)\n\nView Full Changelogs [here]($compare_url)"
