137cac0c858bd58f8654f55712ee7aaebd2f2368
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-29 06:51:59 +0800

    Update __main__.py

--- WebStreamer/__main__.py
+++ WebStreamer/__main__.py
@@ -70,11 +70,26 @@ if __name__ == "__main__":
         logging.info("Stopped Services")
 
 async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+
     url = f'https://dulink.in/api'
-    params = {'api': URL_SHORTNER_WEBSITE_API, 'url': link}
+    params = {'api': URL_SHORTNER_WEBSITE_API,
+              'url': link,        
+              }
 
-
-async with aiohttp.ClientSession() as session:
+    try:
+        async with aiohttp.ClientSession() as session:
             async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
                 data = await response.json()
+                if data["status"] == "success":
                     return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
