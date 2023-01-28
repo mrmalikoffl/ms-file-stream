659c032d492e4da3b9147798dbcbf5f1958ea685
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-29 00:12:51 +0800

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -6,7 +6,7 @@ from pyrogram import filters, errors
 from WebStreamer.vars import Var
 from urllib.parse import quote_plus
 from WebStreamer.bot import StreamBot, logger
-from WebStreamer.utils import get_hash, get_name, get_media_file_size
+from WebStreamer.utils.file_properties import get_name, get_hash, get_media_file_size
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
