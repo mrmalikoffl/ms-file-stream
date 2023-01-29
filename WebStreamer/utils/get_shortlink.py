8002db106cba3dae25bd92317e9a20a66146dc3a
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-29 10:01:19 +0800

    Create get_shortlink.py

---
+++ WebStreamer/utils/get_shortlink.py
@@ -0,0 +1,26 @@
+
+
+async def get_shortlink(link):
+    https = link.split(":")[0]
+    if "http" == https:
+        https = "https"
+        link = link.replace("http", https)
+
+    url = f'https://dulink.in/api'
+    params = {'api': URL_SHORTNER_WEBSITE_API,
+              'url': link,        
+              }
+
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, params=params, raise_for_status=True, ssl=False) as response:
+                data = await response.json()
+                if data["status"] == "success":
+                    return data['shortenedUrl']
+                else:
+                    logger.error(f"Error: {data['message']}")
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
+
+    except Exception as e:
+        logger.error(e)
+        return f'{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
