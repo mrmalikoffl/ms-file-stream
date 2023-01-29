d112b70f4135562c87b317acb607a9004d1040f4
mrmalikoffl <121479735+mrmalikoffl@users.noreply.github.com>
2023-01-29 09:05:51 +0800

    Update start.py

--- WebStreamer/bot/plugins/start.py
+++ WebStreamer/bot/plugins/start.py
@@ -10,7 +10,3 @@ async def start(_, m: Message):
     await m.reply(
         f'Hi {m.from_user.mention(style="md")}, Send me a file to get an instant stream link.'
     )
-    if START_PIC:
-        await message.reply_photo(START_PIC, caption=txt, reply_markup=button)       
-    else:
-        await message.reply_text(text=txt, reply_markup=button, disable_web_page_preview=True)
